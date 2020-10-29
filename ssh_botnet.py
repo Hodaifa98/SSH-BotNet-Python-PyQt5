5d7ffd32250db2a6efca60c42d9c04cffee25810
Hodaifa98 <40869908+Hodaifa98@users.noreply.github.com>
2020-10-29 03:38:33 +0100

    Add files via upload

---
+++ ssh_botnet.py
@@ -0,0 +1,65 @@
+#Import required modules.
+#from pexpect import pxsshS
+import PyQt5
+
+botNet = []
+
+class SSHBotNetApp:
+    def __init__(self):
+        #1: Create a builder
+        self.builder = builder = pygubu.Builder()
+
+        #2: Load an ui file
+        builder.add_from_file('ssh_botnet.ui')
+
+        #3: Create the mainwindow
+        self.mainwindow = builder.get_object('mainwindow')
+        
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+class Client:
+    #
+    def __init__(self, host , user, password, port=22):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.port = port
+        self.session = self.connect()
+
+    #
+    def connect(self):
+        try:
+            s = pxssh.pxssh()
+            s.login(self.host, self.user, self.password, port=self.port)
+            return s
+        except Exception as ex:
+            print("Error connecting...")
+            print(ex)
+
+    #
+    def send_command(self, command):
+        try:
+            self.session.sendline(command)
+            self.session.prompt()
+            return self.session.before
+        except Exception as ex:
+            print(ex)
+
+#
+def sshbotnetCommand(command):
+    for client in botNet:
+        output = client.send_command(command)
+        print("Output from " + client.host)
+        print(output)
+
+#
+def addClient(host, user, password, port=22):
+    client = Client(host, user, password, port)
+    botNet.append(client)
+
+#
+if __name__ == '__main__':
+    addClient("127.0.0.1", "osboxes", "osboxes.org")
+    #sshbotnetCommand("ls")
