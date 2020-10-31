4cde8509b6acd8112bd732c1e4fc59809f1913e6
Hodaifa98 <40869908+Hodaifa98@users.noreply.github.com>
2020-10-31 17:24:14 +0100

    Added threading.

--- ssh_botnet.py
+++ ssh_botnet.py
@@ -1,5 +1,6 @@
 #Import required modules.
 import sys
+import threading
 import paramiko
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QTableWidgetItem
@@ -15,6 +16,12 @@ class Client:
         self.username = username
         self.password = password
         self.port = port
+        try:
+            threading.Thread(target=self.connectToHost).start()
+        except Exception as ex:
+            print(ex)
+
+    def connectToHost(self):
         self.session = self.connect()
 
     #
