23566e93c8a302fa9c11777da65f3f32525b4a3e
Hodaifa98 <40869908+Hodaifa98@users.noreply.github.com>
2020-10-31 16:46:53 +0100

    Added method ro remove row.

--- ssh_botnet.py
+++ ssh_botnet.py
@@ -69,6 +69,9 @@ class Ui_SSHBotNetWindow(object):
             hostObj = dialog.ui.getHostObj()
             self.addHostToListAndRow(hostObj)
 
+    def deleteSelectedRowBtnClick(self):
+        pass
+
     def executeCommand(self):
         sshbotnetCommand(self.commandTextInput.toPlainText())
 
