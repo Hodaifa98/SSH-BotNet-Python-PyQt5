ab3f7d96f248b3cc10ba1c552721db95d0f174ca
Hodaifa98 <40869908+Hodaifa98@users.noreply.github.com>
2020-10-31 16:46:12 +0100

    Removed delete row method (wrong file).

--- Design/add_host_dialog.py
+++ Design/add_host_dialog.py
@@ -22,9 +22,6 @@ class Ui_addHostDialog(object):
         except Exception as ex:
             print(ex)
 
-    def deleteSelectedRowBtnClick(self):
-        pass
-
     def setupUi(self, addHostDialog):
         addHostDialog.setObjectName("addHostDialog")
         addHostDialog.resize(350, 170)
